4ffa100483ec82e00af80a9d84652cef483e4fa8
melroser <themelroser@gmail.com>
2025-08-29 13:10:42 -0400

    adding telelmetry

---
+++ app/instrumentation-client.js
@@ -0,0 +1,17 @@
+
+ 
+import posthog from 'posthog-js'
+
+posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    defaults: '2025-05-24'
+});
+ 
+export function onRouterTransitionStart(url, navigationType) {
+
+    posthog.capture('page_navigation', {
+        url,
+        type: navigationType,
+        timestamp: Date.now(),
+    })
+}
